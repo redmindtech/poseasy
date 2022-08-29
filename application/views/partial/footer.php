9b7988e50b708e47a119fa187c48d1c802912545
redmindtech <69637152+redmindtech@users.noreply.github.com>
2022-08-29 19:07:53 +0530

    inital

---
+++ application/views/partial/footer.php
@@ -0,0 +1,12 @@
+		</div>
+	</div>
+
+	<div id="footer">
+		<div class="jumbotron push-spaces">
+			<strong><?php echo $this->lang->line('common_copyrights', date('Y')); ?> · 
+			<a href="https://redmindtechnologies.com/" target="_blank">RedMind Technologies</a>  · 
+			<?php echo $this->config->item('application_version'); ?></strong>.
+		</div>
+	</div>
+</body>
+</html>
